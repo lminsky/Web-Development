8917362c8fa556ae4637f07455f2c5c5bd08f4f1
Louis Minsky <lminsky@packer.edu>
2018-04-30 07:37:31 -0400

    navigate dom

--- 2017-2018/Class Examples/2018-04-27/Navigate DOM/script.js
+++ 2017-2018/Class Examples/2018-04-27/Navigate DOM/script.js
@@ -2,14 +2,6 @@ document.getElementById("button").addEventListener("click", doSomething);
 
 function doSomething() {
   console.log("Button Pressed");
-  var test = document.getElementById("rainbow");
-  if(test.hasChildNodes()) {
-    test = test.firstChild;
-    while(test.nextSibling != null) {
-      console.log(test);
-      test = test.nextSibling;
-      console.log(test.nodeName)
-    }
-  }
-
+  var list = document.getElementById("rainbow");
+  console.log(list.childNodes);
 }
