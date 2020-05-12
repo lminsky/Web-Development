b66ce612b3cbaf13bdd37469a58ad5be732525dc
Louis Minsky <lminsky@packer.edu>
2020-05-12 10:31:50 -0400

    fixed frustrating indentation

--- docs/chat/script.js
+++ docs/chat/script.js
@@ -4,7 +4,7 @@ login(start, recieveMessage);
 //This function runs after you have logged in. Your user information is passed to you in the variable 'user'
 function start(user) {
 	console.log(user);
-  document.getElementById("sendMessage").addEventListener("click", send);
+	document.getElementById("sendMessage").addEventListener("click", send);
 }
 
 //This function runs every time you recieve a message. It will run for every message already in the database.
